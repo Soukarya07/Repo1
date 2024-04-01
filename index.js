ab0c430dba8ffa68deeab3e72dd6f19c5d840569
Soukarya07 <162974806+Soukarya07@users.noreply.github.com>
2024-04-01 15:57:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Students...')
+  res.send('Hello Soukarya')
 })
 
 app.listen(4000, ()=>{
